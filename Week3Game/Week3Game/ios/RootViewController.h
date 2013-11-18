d254951e972a4f9db5c11673d727f3f276999eb5
cwild@imisshtml.com <cwild@imisshtml.com>
2013-11-17 19:22:52 -0500

    Re-commit of initial+ images... because cocosd2-x has major issues...

---
+++ Week3Game/Week3Game/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  Week3GameAppController.h
+//  Week3Game
+//
+//  Created by Claude Wild on 11/17/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
